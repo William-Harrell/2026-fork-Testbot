339043bb764d7e0edc8b56343ab36cff7bf8b399
Richard Scott <bububooo3@gmail.com>
2026-03-04 17:42:55 -0500

    dsfhgj

--- src/main/java/frc/robot/auto/AutoSetup.java
+++ src/main/java/frc/robot/auto/AutoSetup.java
@@ -1,32 +1,34 @@
 package frc.robot.auto;
 
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.pathplanner.lib.config.ModuleConfig;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.path.PathConstraints;
-import frc.robot.util.constants;
+import frc.robot.util.constants.RobotPhysicalConstants;
+import frc.robot.util.constants.DrivingConstants;
+import frc.robot.subsystems.swerve.SwereConstants;
 
 public class AutoSetup {
   public AutoSetup() {
-    CSPPathing.setRobotDimensions(1 / 2);
+    // Pythagorean theorem for diagonal length
+    CSPPathing.setRobotDimensions(Math.sqrt(
+        Math.pow(RobotPhysicalConstants.ROBOT_LENGTH, 2) +
+            Math.pow(RobotPhysicalConstants.ROBOT_WIDTH, 2)));
 
-CSPPathing.addObstacle(cornerPose, nonAdjacentCornerPose);
+    CSPPathing.addObstacle(cornerPose, nonAdjacentCornerPose);
 
-CSPPathing.configureConstraints(pathConstraints, robotConfig);
+    CSPPathing.configureConstraints(pathConstraints, robotConfig);
   }
-// template pathconstraints
+
+  // template pathconstraints
 public PathConstraints pathConstraints = new PathConstraints(
-DRIVE_MAX_VEL, DRIVE_MAX_ACC,
-TURN_MAX_VEL, TURN_MAX_ACC);
+DrivingConstants.DRIVE_MAX_VEL, DrivingConstants.DRIVE_MAX_ACC,
+DrivingConstants.TURN_MAX_VEL, DrivingConstants.TURN_MAX_ACC);
 
-public RobotConfig robotConfig = new RobotConfig(MASS, MOI,
-new ModuleConfig(WHEELRADIUS, MAX_VEL, COEFFICIENT_OF_FRICTION,
-driveMotor, CURRENT_LIMIT, NUM_OF_MOTORS),
-moduleOffsets);
+  public RobotConfig robotConfig = new RobotConfig(MASS, MOI,
+      new ModuleConfig(, MAX_VEL, COEFFICIENT_OF_FRICTION,
+          driveMotor, CURRENT_LIMIT, NUM_OF_MOTORS),
+      moduleOffsets);
 
-CSPPathing.configureNodes(
-  CSPPathing.NavNode(idString1, nodePose1, List.of(idString2, idString3)),
-  CSPPathing.NavNode(idString2, nodePose2, List.of(idString1, idString3)),
-  CSPPathing.NavNode(idString3, nodePose3, List.of(idString1, idString2)),
-  ...
-);
+  CSPPathing.configureNodes(CSPPathing.NavNode(idString1,nodePose1,List.of(idString2,idString3)),CSPPathing.NavNode(idString2,nodePose2,List.of(idString1,idString3)),CSPPathing.NavNode(idString3,nodePose3,List.of(idString1,idString2)),...);
 }
--- src/main/java/frc/robot/util/constants/DrivingConstants.java
+++ src/main/java/frc/robot/util/constants/DrivingConstants.java
@@ -7,4 +7,10 @@ public final class DrivingConstants {
       true; // Motor runs @ % (true) or exact speed w/ encoder (false)
   public static final double NORMAL_SPEED_MULTIPLIER = 1.0; // Scale according to joystick
   public static final double SLOW_SPEED_MULTIPLIER = 0.3;
+
+  public static final double DRIVE_MAX_VEL = 0.0; 
+  public static final double DRIVE_MAX_ACC = 0.0; // TODO (placeholder)
+
+  public static final double TURN_MAX_VEL = 0.0; 
+  public static final double TURN_MAX_ACC = 0.0; // TODO (placeholder)
 }

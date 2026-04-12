8782645e36f6591f700c54d3cbb826992834de74
Will <willthemcer@gmail.com>
2026-04-12 11:39:07 -0400

    kill useless dependency

--- src/main/java/frc/robot/subsystems/intake/Roller.java
+++ src/main/java/frc/robot/subsystems/intake/Roller.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;

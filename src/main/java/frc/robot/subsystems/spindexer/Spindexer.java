4065fcfc96af4ab2002fc33d44a0de94542cdaa2
Will <willthemcer@gmail.com>
2026-04-15 16:42:53 -0400

    Fixed

--- src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
@@ -1,15 +1,8 @@
 package frc.robot.subsystems.spindexer;
 
-import com.revrobotics.PersistMode;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SparkFlexConfig;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.intake.IntakeConstants;
 import frc.robot.subsystems.spindexer.SpindexerState.spindexer_state;
 
 public class Spindexer extends SubsystemBase{
@@ -19,17 +12,6 @@ public class Spindexer extends SubsystemBase{
     public Spindexer() {
         state = new SpindexerState();
         spinner = new Spinner(new SparkFlex(SpindexerConstants.SPINDEXER_ID, MotorType.kBrushless));
-
-    SparkFlexConfig config = new SparkFlexConfig();
-      config.idleMode(SpindexerConstants.SPIN_COAST ? 
-        IdleMode.kCoast : IdleMode.kBrake)
-        .smartCurrentLimit(
-          SpindexerConstants.SPIN_STATOR_CURRENT_LIMIT, 
-          SpindexerConstants.SPIN_SUPPLY_CURRENT_LIMIT);
-
-        spinner.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
-        spinner.
-
     }
 
     public void startFeed() {
